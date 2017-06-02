91fe083d502c4feaa1f107203cb733aa90219d11
Daniel Daniil <dannyd1997@g.ucla.edu>
2017-06-02 01:47:23 -0700

    added arg

--- lab3b.py
+++ lab3b.py
@@ -1,1 +1,8 @@
-print "Hello World!"
+#!usr/bin/python
+
+import sys, string,argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_system")
+args = parser.parse_args()
+print args.file_system
