d2dc5f9ff59570107db203bc67cdd2f9fe05a843
richardyu123 <richard95yu@gmail.com>
2017-06-02 02:32:09 -0700

    another minor change

--- lab3b.py
+++ lab3b.py
@@ -8,6 +8,7 @@ def main():
     file_system = ""
     if len(sys.argv) != 2:
         print >> sys.stderr, "Usage: python2 lab3b.py file_system"
+        sys.exit(1)
     else:
         file_system = sys.argv[1]
         print file_system
